1da124ae64e346009d651ab46fa2f92f770b76ac
Syddev <sydneysantos176@gmail.com>
2025-11-29 10:13:25 +0800

    install lucide react

---
+++ my-portfolio/src/components/portfolio/ProfileCard.jsx
@@ -0,0 +1,10 @@
+import ProfilePic from "../../assets/syd.jpg";
+import { MapPin, Mail, FileText, Github } from "lucide-react";
+
+const ProfileCard = () => {
+  return (
+    <div className="rounded-2xl bg-card shadow-bento-lg p-6 md:p-8 border border-border gradient-card"></div>
+  );
+};
+
+export default ProfileCard;
---
+++ my-portfolio/src/pages/Index.jsx
@@ -0,0 +1,11 @@
+import ProfileCard from "../components/portfolio/ProfileCard";
+
+const Index = () => {
+  return (
+    <div className="">
+      <ProfileCard />
+    </div>
+  );
+};
+
+export default Index;
