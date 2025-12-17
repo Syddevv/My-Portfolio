4e6fc92637977dd5580cf06c0cede4c858f22f90
Syddev <sydneysantos176@gmail.com>
2025-12-17 21:31:04 +0800

    seperated projects data

---
+++ my-portfolio/src/data/projectsData.jsx
@@ -0,0 +1,55 @@
+import LetemCook from "../assets/LetemCook.png";
+import SpenSyd from "../assets/SpenSyd.png";
+import Orbit from "../assets/Orbit.png";
+import CraftMySite from "../assets/CraftMySite.png";
+
+export const projectsData = [
+  {
+    title: "SpenSyd",
+    desc: "Personal Finance Tracker with AI integration",
+    fulldesc:
+      "SpenSyd is a modern web application built to help users track their spending and income efficiently powered by a smart AI assistant.",
+    tags: ["MERN Stack", "Gemini API", "Tailwind"],
+    link: "https://spensyd.vercel.app",
+    color: "text-blue-500",
+    bg: "bg-blue-500/10",
+    image: SpenSyd,
+    github: "https://github.com/Syddevv/SpenSyd.git",
+  },
+  {
+    title: "Let'em Cook",
+    desc: "Community Recipe Sharing Platform",
+    fulldesc:
+      "Let'em Cook is a vibrant online platform designed for passionate home cooks to share their culinary masterpieces.",
+    tags: ["MERN Stack"],
+    link: "https://letem-cook.vercel.app",
+    color: "text-orange-500",
+    bg: "bg-orange-500/10",
+    image: LetemCook,
+    github: "https://github.com/Syddevv/LetemCook.git",
+  },
+  {
+    title: "CraftMySite",
+    desc: "Template Marketplace & Custom Web Services",
+    fulldesc:
+      "Comprehensive web platform bridging the gap between professional web development services and customizable digital products.",
+    tags: ["PHP", "MySQL", "Gemini Api"],
+    link: "https://github.com/Syddevv/CraftMySite",
+    color: "text-purple-500",
+    bg: "bg-purple-500/10",
+    image: CraftMySite,
+    github: "https://github.com/Syddevv/CraftMySite.git",
+  },
+  {
+    title: "Orbit",
+    desc: "Modern Anonymous Chat App",
+    fulldesc:
+      "Orbit is a modern, anonymous chat application designed for serendipitous connections.",
+    tags: ["React", "Tailwind", "Node.js", "Socket.io", "Mongoose"],
+    link: "https://orbit-chat-web.vercel.app",
+    color: "text-purple-500",
+    bg: "bg-purple-500/10",
+    image: Orbit,
+    github: "https://github.com/Syddevv/Orbit.git",
+  },
+];
