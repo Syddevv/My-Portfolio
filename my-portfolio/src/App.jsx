384a5e7eb7461a1fa698677de7dc17ee35e9f151
Syddev <sydneysantos176@gmail.com>
2026-01-02 20:03:07 +0800

    refactor: comment out unused snowfall effect code

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,35 +3,35 @@ import Index from "./pages/Index";
 import { ToastContainer } from "react-toastify";
 import Projects from "./pages/Projects";
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Snowfall from "react-snowfall";
 
 function App() {
-  const [snowColor, setSnowColor] = useState("#fff");
+  // const [snowColor, setSnowColor] = useState("#fff");
 
-  useEffect(() => {
-    const updateSnowColor = () => {
-      const isDark = document.documentElement.classList.contains("dark");
-      setSnowColor(isDark ? "#ffffff" : "#00b7eb  ");
-    };
+  // useEffect(() => {
+  //   const updateSnowColor = () => {
+  //     const isDark = document.documentElement.classList.contains("dark");
+  //     setSnowColor(isDark ? "#ffffff" : "#00b7eb  ");
+  //   };
 
-    updateSnowColor();
+  //   updateSnowColor();
 
-    const observer = new MutationObserver(() => {
-      updateSnowColor();
-    });
+  //   const observer = new MutationObserver(() => {
+  //     updateSnowColor();
+  //   });
 
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ["class"],
-    });
+  //   observer.observe(document.documentElement, {
+  //     attributes: true,
+  //     attributeFilter: ["class"],
+  //   });
 
-    return () => observer.disconnect();
-  }, []);
+  //   return () => observer.disconnect();
+  // }, []);
 
   return (
     <div>
-      <Snowfall
+      {/* <Snowfall
         style={{
           position: "fixed",
           width: "100vw",
@@ -46,7 +46,7 @@ function App() {
         speed={[1, 2.5]}
         wind={[1, 2.5]}
         radius={[0.5, 3, 5]}
-      />
+      /> */}
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/projects" element={<Projects />} />
